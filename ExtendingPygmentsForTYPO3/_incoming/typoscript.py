bd41dc076c0c4b08dadac4deeb6566406987efcc
Martin Bless <martin.bless@gmail.com>
2013-07-12 12:04:04 +0200

    [TASK] Update to v1.1.3 of TypoScript lexer for Pygments highlighter
    Change-Id: I574859bb7de0f70904a13bc46cabe330e5d16051
    Related: #49880
    Reviewed-on: https://review.typo3.org/22238
    Reviewed-by: Martin Bless
    Tested-by: Martin Bless

--- ExtendingPygmentsForTYPO3/_incoming/typoscript.py
+++ ExtendingPygmentsForTYPO3/_incoming/typoscript.py
@@ -3,7 +3,18 @@
     pygments.lexers.typoscript
     ~~~~~~~~~~~~~~~~~~~~~~
 
-    Pygments lexers for TypoScript
+    Lexers for TypoScript
+
+    `TypoScriptLexer`
+        A TypoScript lexer.
+
+    `TypoScriptCssDataLexer`
+        Lexer that highlights markers, constants and registers within css.
+
+    `TypoScriptHtmlDataLexer`
+        Lexer that highlights markers, constants and registers within html tags.
+
+    version: 1.1.3
 
     :copyright: Copyright 2013-now by Donation Based Hosting.
     :license: BSD, see LICENSE for details.
@@ -17,96 +28,121 @@ from pygments.lexer import DelegatingLexer
 from pygments.lexers.web import HtmlLexer, CssLexer
 
 
-__all__ = ['TypoScriptLexer', 'TypoScriptCssLexer', 'HtmlTypoScriptLexer',
-           'TypoScriptDataLexer']
+__all__ = ['TypoScriptLexer', 'TypoScriptCssDataLexer', 'TypoScriptHtmlDataLexer']
 
-class TypoScriptCssLexer(DelegatingLexer):
+class TypoScriptCssDataLexer(RegexLexer):
     """
-    Subclass of the `VelocityLexer` that highlights unlexer data
-    with the `CssLexer`.
+    Lexer that highlights markers, constants and registers within css blocks.
 
     """
-    def __init__(self, **options):
-        super(TypoScriptCssLexer, self).__init__(TypoScriptLexer, CssLexer,
-                                              **options)
-
-class HtmlTypoScriptLexer(DelegatingLexer):
-    """
-    Subclass of the `TypoScriptDataLexer` that highlights unlexer data
-    with the `HtmlLexer`.
-
-    """
-    def __init__(self, **options):
-        super(HtmlTypoScriptLexer, self).__init__(TypoScriptDataLexer, HtmlLexer,
-                                              **options)
-
-class TypoScriptDataLexer(RegexLexer):
-    """
-    """
-    name = 'TypoScriptData'
-    aliases = ['typoscriptdata']
+    name = 'TypoScriptCssData'
+    aliases = ['typoscriptcssdata']
 
     tokens = {
         'root': [
-            # marker: ###MARK###
+              # marker: ###MARK###
             (r'(.*)(###\w+###)(.*)', bygroups(String, Name.Constant, String)),
-            # constant: {$some.constant}
-            (r'(.*)(\{)(\$)([\w-]+\.)*([\w-]+)(\})(.*)',
-             bygroups(String, String.Symbol, Operator, Name.Constant, Name.Constant, String.Symbol, String)), # constant
-            # constant: {register:somevalue}
-            (r'(.*)(\{)([\w-]+(?:(\:)))*([\w-]+)(\})(.*)',
-             bygroups(String, String.Symbol, Name.Constant, Operator, Name.Constant, String.Symbol, String)), # constant
-            (r'<[^\s][^\n>]*>', String),
+              # constant: {$some.constant}
+            (r'(\{)(\$)((?:[\w\-_]+\.)*)([\w\-_]+)(\})',
+                bygroups(String.Symbol, Operator, Name.Constant, Name.Constant, String.Symbol)), # constant
+              # constant: {register:somevalue}
+            (r'(.*)(\{)([\w\-_]+)(\s*:\s*)([\w\-_]+)(\})(.*)',
+                bygroups(String, String.Symbol, Name.Constant, Operator, Name.Constant, String.Symbol, String)), # constant
+              # whitespace
+            (r'\s+', Text),
+              # comments
+            (r'/\*(?:(?!\*/).)*\*/', Comment),
+            (r'(?<!(#|\'|"))(?:#(?!(?:[a-fA-F0-9]{6}|[a-fA-F0-9]{3}))[^\n#]+|//[^\n]*)', Comment),
+              # other
+            (r'[<>,:=\.\*%+\|]', String),
+            (r'[\w"_\-!\/&;\(\)\{\}]+', String),
+        ]
+    }
+
+
+class TypoScriptHtmlDataLexer(RegexLexer):
+    """
+    Lexer that highlights markers, constants and registers within html tags.
+
+    """
+    name = 'TypoScriptHtmlData'
+    aliases = ['typoscripthtmldata']
+
+    tokens = {
+        'root': [
+              # INCLUDE_TYPOSCRIPT
+            (r'(INCLUDE_TYPOSCRIPT)', Name.Class),
+              # Language label or extension resource FILE:... or LLL:... or EXT:...
+            (r'(EXT|FILE|LLL):[^\}\n"]*', String),
+              # marker: ###MARK###
+            (r'(.*)(###\w+###)(.*)', bygroups(String, Name.Constant, String)),
+              # constant: {$some.constant}
+            (r'(\{)(\$)((?:[\w\-_]+\.)*)([\w\-_]+)(\})',
+                bygroups(String.Symbol, Operator, Name.Constant, Name.Constant, String.Symbol)), # constant
+              # constant: {register:somevalue}
+            (r'(.*)(\{)([\w\-_]+)(\s*:\s*)([\w\-_]+)(\})(.*)',
+                bygroups(String, String.Symbol, Name.Constant, Operator, Name.Constant, String.Symbol, String)), # constant
+              # whitespace
+            (r'\s+', Text),
+              # other
+            (r'[<>,:=\.\*%+\|]', String),
+            (r'[\w"_\-!\/&;\(\)\{\}#]+', String),
         ]
     }
 
 class TypoScriptLexer(RegexLexer):
     """
+    Lexer for TypoScript code.
 
+    http://docs.typo3.org/typo3cms/TyposcriptReference/
     """
     name = 'TypoScript'
     aliases = ['typoscript']
     filenames = ['*.ts','*.txt']
     mimetypes = ['text/typoscript']
+
     flags = re.DOTALL | re.MULTILINE
+
     tokens = {
         'root': [
             include('comment'),
             include('constant'),
             include('html'),
-            include('punctuation'),
-            include('operator'),
             include('label'),
             include('whitespace'),
             include('keywords'),
+            include('punctuation'),
+            include('operator'),
             include('structure'),
             include('literal'),
             include('other'),
         ],
         'keywords': [
-            # Conditions
+              # Conditions
             (r'(\[)(?i)(browser|compatVersion|dayofmonth|dayofweek|dayofyear|device|ELSE|END|GLOBAL|globalString|globalVar|hostname|hour|IP|language|loginUser|loginuser|minute|month|page|PIDinRootline|PIDupinRootline|system|treeLevel|useragent|userFunc|usergroup|version)([^\]]*)(\])', bygroups(String.Symbol, Name.Constant, Text, String.Symbol)),
-            # Functions
-            (r'(?=[\w-])(HTMLparser|HTMLparser_tags|addParams|cache|encapsLines|filelink|if|imageLinkWrap|imgResource|makelinks|numRows|numberFormat|parseFunc|replacement|round|select|split|stdWrap|strPad|tableStyle|tags|textStyle|typolink)(?![\w-])', Name.Function),
-            # Toplevel objects FIXME
-            (r'(?:(=\s*<?\s*)|(^\s*))(cObj|field|config|content|constants|FEData|file|frameset|includeLibs|lib|page|plugin|register|resources|sitemap|sitetitle|styles|temp|tt_[^\.]*|types|xmlnews|_GIFBUILDER)(?![\w-])', Name.Builtin),
-            # Content objects
-            (r'(?=[\w-])(CASE|CLEARGIF|COA|COA_INT|COBJ_ARRAY|COLUMNS|CONTENT|CTABLE|EDITPANEL|FILE|FILES|FLUIDTEMPLATE|FORM|HMENU|HRULER|HTML|IMAGE|IMGTEXT|IMG_RESOURCE|LOAD_REGISTER|MEDIA|MULTIMEDIA|OTABLE|QTOBJECT|RECORDS|RESTORE_REGISTER|SEARCHRESULT|SVG|SWFOBJECT|TEMPLATE|TEXT|USER|USER_INT)(?![\w-])', Name.Class),
-            # Menu states
-            (r'(?=[\w-])(ACT|ACTIFSUB|ACTIFSUBRO|ACTRO|CUR|CURIFSUB|CURIFSUBRO|CURRO|IFSUB|IFSUBRO|NO|SPC|USERDEF1|USERDEF1RO|USERDEF2|USERDEF2RO|USR|USRRO)', Name.Class),
-            # Menu objects
-            (r'(?=[\w-])(GMENU|GMENU_FOLDOUT|GMENU_LAYERS|IMGMENU|IMGMENUITEM|JSMENU|JSMENUITEM|TMENU|TMENUITEM|TMENU_LAYERS)', Name.Class),
-            # PHP objects
-            (r'(?=[\w-])(PHP_SCRIPT(_EXT|_INT)?)', Name.Class),
-            (r'(?=[\w-])(userFunc)(?![\w-])', Name.Function),
+              # Functions
+            (r'(?=[\w\-_])(HTMLparser|HTMLparser_tags|addParams|cache|encapsLines|filelink|if|imageLinkWrap|imgResource|makelinks|numRows|numberFormat|parseFunc|replacement|round|select|split|stdWrap|strPad|tableStyle|tags|textStyle|typolink)(?![\w\-_])', Name.Function),
+              # Toplevel objects and _*
+            (r'(?:(=?\s*<?\s+|^\s*))(cObj|field|config|content|constants|FEData|file|frameset|includeLibs|lib|page|plugin|register|resources|sitemap|sitetitle|styles|temp|tt_[^:\.\n\s]*|types|xmlnews|INCLUDE_TYPOSCRIPT|_CSS_DEFAULT_STYLE|_DEFAULT_PI_VARS|_LOCAL_LANG)(?![\w\-_])',
+              bygroups(Operator, Name.Builtin)),
+              # Content objects
+            (r'(?=[\w\-_])(CASE|CLEARGIF|COA|COA_INT|COBJ_ARRAY|COLUMNS|CONTENT|CTABLE|EDITPANEL|FILE|FILES|FLUIDTEMPLATE|FORM|HMENU|HRULER|HTML|IMAGE|IMGTEXT|IMG_RESOURCE|LOAD_REGISTER|MEDIA|MULTIMEDIA|OTABLE|QTOBJECT|RECORDS|RESTORE_REGISTER|SEARCHRESULT|SVG|SWFOBJECT|TEMPLATE|TEXT|USER|USER_INT)(?![\w\-_])', Name.Class),
+              # Menu states
+            (r'(?=[\w\-_])(ACT|ACTIFSUB|ACTIFSUBRO|ACTRO|CUR|CURIFSUB|CURIFSUBRO|CURRO|IFSUB|IFSUBRO|NO|SPC|USERDEF1|USERDEF1RO|USERDEF2|USERDEF2RO|USR|USRRO)', Name.Class),
+              # Menu objects
+            (r'(?=[\w\-_])(GMENU|GMENU_FOLDOUT|GMENU_LAYERS|IMGMENU|IMGMENUITEM|JSMENU|JSMENUITEM|TMENU|TMENUITEM|TMENU_LAYERS)', Name.Class),
+              # PHP objects
+            (r'(?=[\w\-_])(PHP_SCRIPT(_EXT|_INT)?)', Name.Class),
+            (r'(?=[\w\-_])(userFunc)(?![\w\-_])', Name.Function),
         ],
         'whitespace': [
             (r'\s+', Text),
         ],
         'html':[
-            (r'<[^\s][^\n>]*>', using(TypoScriptDataLexer)),
+            (r'<[^\s][^\n>]*>', using(TypoScriptHtmlDataLexer)),
             (r'&[^;\n]*;', String),
-            (r'(?=_CSS_DEFAULT_STYLE\s*\()(?s)(.*(?=\n\)))', using(TypoScriptCssLexer)),
+            (r'(_CSS_DEFAULT_STYLE)(\s*)(\()(?s)(.*(?=\n\)))',
+              bygroups(Name.Class, Text, String.Symbol, using(TypoScriptCssDataLexer))),
         ],
         'literal': [
             (r'0x[0-9A-Fa-f]+t?',Number.Hex),
@@ -115,36 +151,38 @@ class TypoScriptLexer(RegexLexer):
             (r'(###\w+###)', Name.Constant),
         ],
         'label': [
-            # Language label or extension resource LLL:... / EXT:...
-            (r'(EXT|LLL):[^\}\n]*', String),
-            # Path to a resource
-            (r'(?![^\w-])([\w-]+(/[\w-]+)+/?)([^\s]*\n)', bygroups(String, String, String)),
+              # Language label or extension resource FILE:... or LLL:... or EXT:...
+            (r'(EXT|FILE|LLL):[^\}\n"]*', String),
+              # Path to a resource
+            (r'(?![^\w\-_])([\w\-_]+(?:/[\w\-_]+)+/?)([^\s]*\n)', bygroups(String, String)),
         ],
         'punctuation': [
             (r'[,\.]', Punctuation),
         ],
         'operator': [
-            (r'[<>,:=\.\*%+\|-]', Operator),
+            (r'[<>,:=\.\*%+\|]', Operator),
         ],
         'structure': [
-            # Brackets and braces
+              # Brackets and braces
             (r'[\{\}\(\)\[\]]', String.Symbol),
         ],
         'constant': [
-            # Constant: {$some.constant}
-            (r'(\{)(\$)((?:[\w-]+\.)*)([\w-]+)(\})',
-             bygroups(String.Symbol, Operator, Name.Constant, Name.Constant, String.Symbol)), # constant
-            # Constant: {register:somevalue}
-            (r'(\{)([\w-]+(?:(\:)))*([\w-]+)(\})',
-             bygroups(String.Symbol, Name.Constant, Operator, Name.Constant, String.Symbol)), # constant
-            # Hex color: #ff0077
+              # Constant: {$some.constant}
+            (r'(\{)(\$)((?:[\w\-_]+\.)*)([\w\-_]+)(\})',
+                bygroups(String.Symbol, Operator, Name.Constant, Name.Constant, String.Symbol)), # constant
+              # Constant: {register:somevalue}
+            (r'(\{)([\w\-_]+)(\s*:\s*)([\w\-_]+)(\})',
+                bygroups(String.Symbol, Name.Constant, Operator, Name.Constant, String.Symbol)), # constant
+              # Hex color: #ff0077
             (r'(#[a-fA-F0-9]{6}\b|#[a-fA-F0-9]{3}\b)', String.Char)
         ],
         'comment': [
             (r'(?<!(#|\'|"))(?:#(?!(?:[a-fA-F0-9]{6}|[a-fA-F0-9]{3}))[^\n#]+|//[^\n]*)', Comment),
-            (r'(^\s*#\s?\n)', Comment),
+            (r'/\*(?:(?!\*/).)*\*/', Comment),
+            (r'(\s*#\s*\n)', Comment),
         ],
         'other': [
-            (r'[\w"\{\}&;\/\\!\&]+', Text),
+            (r'[\w"\-_!\/&;]+', Text),
         ],
     }
+
