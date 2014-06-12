1c8fce11d96d8892e104360334a283ddf0e9f255
Martin Bless <martin.bless@gmail.com>
2014-06-12 13:03:59 +0200

    [TASK] Rework GlobalSettings.py, empty the intersphinx_mapping
    Move more global settings from conf.py to GlobalSettings.py.
    Make 'intersphinx_mapping' empty.
    Add settings for LaTeX pdf generation.
    Improve the TYPO3 specific codeblock.
    
    Releases: 0.0
    Resolves: #59522, #51854
    Change-Id: Ib91e13561b55c1cf765d0bb0acd94bb9b45a6df0
    Reviewed-on: https://review.typo3.org/30686
    Reviewed-by: Martin Bless
    Tested-by: Martin Bless

--- ExtendingSphinxForTYPO3/src/t3sphinx/resources/typo3_codeblock_for_conf.py
+++ ExtendingSphinxForTYPO3/src/t3sphinx/resources/typo3_codeblock_for_conf.py
@@ -1,26 +1,14 @@
-# -*- coding: utf-8 -*-
-#
-
-# Example
-
-# Project name documentation build configuration file, ...0
-
-project = 'Some project name'
-version = '1.2'
-
-
-
-
-#=================================================
-#
+#=======================================================================
 # TYPO3 codeblock BEGIN:
 #
-# Insert this codeblock at the end of your Sphinx
-# builder configuration file 'conf.py'.
-# This may enable TYPO3 specific features like
-# TYPO3 themes. It makes Yaml settings files work.
-# 
-#-------------------------------------------------
+# Insert this codeblock at the end of your Sphinx builder configuration
+# file 'conf.py'. This may enable TYPO3 specific features like TYPO3
+# themes. It makes Yaml settings files work.
+#
+# Two lines are marked with '# check this!'. Make sure they specify the
+# relative path from this 'conf.py' file to the master document and to
+# the folder where logfiles are written.
+#-----------------------------------------------------------------------
 
 if 1 and "TYPO3 specific":
 
@@ -46,13 +34,13 @@ if 1 and "TYPO3 specific":
 
     t3DocTeam['conf_py_package_dir'] = os.path.abspath(os.path.dirname(
         t3DocTeam['conf_py_file']))
-    t3DocTeam['relpath_to_master_doc'] = '..'
-    t3DocTeam['relpath_to_logdir'] = '_not_versioned'
+    t3DocTeam['relpath_to_master_doc'] = '..'             # check this!
+    t3DocTeam['relpath_to_logdir'] = '_not_versioned'     # check this!
     t3DocTeam['path_to_logdir'] = os.path.join(
         t3DocTeam['conf_py_package_dir'],
         t3DocTeam['relpath_to_logdir'])
     t3DocTeam['pathToYamlSettings'] = os.path.join(
-        t3DocTeam['conf_py_package_dir'], 
+        t3DocTeam['conf_py_package_dir'],
         t3DocTeam['relpath_to_master_doc'], 'Settings.yml')
     try:
         t3DocTeam['pathToGlobalYamlSettings'] = \
@@ -75,9 +63,6 @@ if 1 and "TYPO3 specific":
     if __function:
         __function(globals(), t3DocTeam)
 
-#-------------------------------------------------
-#
+#-----------------------------------------------------------------------
 # TYPO3 codeblock END.
-# 
-#=================================================
-
+#=======================================================================
